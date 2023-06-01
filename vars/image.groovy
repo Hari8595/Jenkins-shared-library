18416e734176efe35c8e387d7ea60d467d1cbf7f
hari <harivishnumurugan@gmail.com>
2023-06-01 13:58:16 +0530

    Five

--- vars/image.groovy
+++ vars/image.groovy
@@ -3,6 +3,6 @@ def call(String imageName) {
     withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
         sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USR --password-stdin"
-        sh "docker $imageName"
+        sh "docker push $imageName"
     }
 }
