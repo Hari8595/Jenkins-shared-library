137f425ab09292375ff366e4077b11b034804e7f
hari <harivishnumurugan@gmail.com>
2023-06-01 14:33:50 +0530

    Error

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -2,12 +2,13 @@ package com.example
 
 class Docker implements Serializable {
     def script
+
     Docker(script) {
         this.script = script
     }
 
     def image(String imageName) {
-        script.withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
+        script.withCredentials([script.usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
             script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USR --password-stdin"
             script.sh "docker push $imageName"
