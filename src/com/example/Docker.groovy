0001ccd22b0d2b5679294822725f48e1da8e5aea
hari <harivishnumurugan@gmail.com>
2023-06-01 14:28:13 +0530

    sixth

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,16 @@
+package com.example
+
+class Docker implements Serializable {
+    def script
+    Docker(script) {
+        this.script = script
+    }
+
+    def image(String imageName) {
+        script.withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USR --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- vars/image.groovy
+++ vars/image.groovy
@@ -1,8 +1,5 @@
 #!/usr/bin/env groovy
+import com.example.Docker
 def call(String imageName) {
-    withCredentials([usernamePassword(credentialsId: "Docker", usernameVariable: "USR", passwordVariable: "PASS")]) {
-        sh "docker build -t $imageName ."
-        sh "echo $PASS | docker login -u $USR --password-stdin"
-        sh "docker push $imageName"
-    }
+   return new Docker(this).image(imageName)
 }
